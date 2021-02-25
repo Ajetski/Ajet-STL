8e36a9de3f5b4ecd913f48f6d11a09c3a3431a4c
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-02-25 12:56:29 -0500

    add iterators and fix pushback

--- include/list.h
+++ include/list.h
@@ -1,5 +1,5 @@
 /*
-* array.h
+* list.h
 * (c) 2021 Adam Jeniski
 *
 * Attribution and copyright notice must be retained.
@@ -20,10 +20,58 @@
 
 namespace ajet {
     using std::allocator, std::allocator_traits, std::size_t,
-          std::unique_ptr, std::make_unique, std::initializer_list;
+          std::shared_ptr, std::make_shared, std::initializer_list;
+
+    template<typename T>
+    struct list_node {
+        T data;
+        shared_ptr<list_node<T>> next;
+        list_node(T data) : data{ data }, next{ nullptr } {}
+        list_node(const list_node& rhs) : data { rhs.data }, next{ rhs.next } {}
+        list_node operator=(const list_node& rhs) {
+            data = rhs.data;
+            next = rhs.next;
+        }
+    };
+
+    template<typename T>
+    struct list_iterator {
+
+        list_iterator() noexcept = default;
+        list_iterator(list_node<T> *ptr) noexcept : ptr{ ptr } {}
+
+        constexpr list_iterator& operator++() {
+            ptr = ptr->next.get();
+            return *this;
+        }
+
+        constexpr list_iterator& operator++(int) {
+            ptr = ptr->next.get();
+            return *this;
+        }
+
+        constexpr T& operator*() {
+            if (ptr == nullptr) {
+                throw "test";
+            }
+            return ptr->data;
+        }
+        //constexpr T* operator->() const noexcept { return node; }
+
+        constexpr bool operator==(const list_iterator& rhs) const {
+            return this->ptr != rhs.ptr;
+        }
+        constexpr bool operator!=(const list_iterator& rhs) const {
+            return !(this->ptr == rhs.ptr);
+        }
+
+    private:
+        list_node<T>* ptr;
+
+    };
 
     template <class T, class Allocator = allocator<T>>
-	struct list {
+    struct list {
         // types:
         using value_type = T;
         using allocator_type = Allocator;
@@ -32,64 +80,61 @@ namespace ajet {
         using reference = value_type&;
         using const_reference = const value_type&;
         using size_type = size_t;
-      /*using difference_type = ;
-        using iterator = ;
-        using const_iterator = ;
+        using difference_type = ptrdiff_t;
+        using iterator = list_iterator<value_type>;
+        //using const_iterator = list_iterator<const list_node<value_type>;
         using reverse_iterator = std::reverse_iterator<iterator>;
-        using const_reverse_iterator = std::reverse_iterator<const_iterator>;*/
+        //using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
         list() = default;
         
         list(initializer_list<T> vals) {
-            this->assign(vals);
+            assign(vals);
         }
 
-        ~list() {
-
+        void assign(size_type n, const T& t) {
+            auto iter = begin();
+            for (int i = 0; i < n; ++iter, ++i) {}
+            *iter = t;
         }
 
+        iterator begin() noexcept { return iterator( head.get() ); }
+        //const_iterator begin() const noexcept { return iterator(*head); }
 
-        /*void assign(size_type n, const T& t) {
-
-        }*/
+        iterator end() noexcept { return iterator( tail.get()->next.get() ); }
+        //const_iterator end() const noexcept { return iterator(*tail); }
 
         void assign(initializer_list<T> vals) {
             for (T val : vals)
-                this->push_back(val);
+                push_back(val);
         }
 
         void push_back(const T& x) {
-            auto temp = unique_ptr<Node>{ make_unique<Node>(x) };
             if (head == nullptr) {
-                temp->next = std::move(head);
-                head = std::move(temp);
+                head = std::move(shared_ptr<Node>{ make_shared<Node>(x) });
+                tail = head;
             }
             else {
-                temp->next = std::move(tail);
-                tail = std::move(temp);
+                tail->next = std::move(shared_ptr<Node>{ make_shared<Node>(x) });
+                tail = tail->next;
             }
         }
 
         void push_back(T&& x) {
-            auto temp = unique_ptr<Node>{ make_unique<Node>(x) };
             if (head == nullptr){
-                temp->next = std::move(head);
-                head = std::move(temp);
+                head = std::move(shared_ptr<Node>{ make_shared<Node>(x) });
+                tail = head;
             }
             else {
-                temp->next = std::move(tail);
-                tail = std::move(temp);
+                tail->next = std::move(shared_ptr<Node>{ make_shared<Node>(x) });
+                tail = tail->next;
             }
         }
 
     private:
-        struct Node {
-            T data;
-            unique_ptr<Node> next;
-            Node(int data) : data{ data }, next{ nullptr } {}
-        };
-        unique_ptr<Node> head{ nullptr };
-        unique_ptr<Node> tail{ nullptr };
+        using Node = list_node<value_type>;
+        shared_ptr<Node> head{ nullptr };
+        shared_ptr<Node> tail{ nullptr };
 	};
 }
 
